b8f8c3fb8396fc8c6d3681a9f1263c760d4af9fc
hstewart93 <harriet.stt@gmail.com>
2020-04-29 10:37:37 +0100

    Fix check column and gini single tests.

--- tests/test_check_columns.py
+++ tests/test_check_columns.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 from pl_curve import check_columns
-import pandas
+import pandas as pd
 
 
 def test_check_columns_correct():
-    '''FIXME: Implement this test
+    '''
     Tests check_columns correctly checks items summing to 1.0 returns true
     '''
-    raise NotImplementedError("Please implement this test")
+
+    df = pd.DataFrame([0.1, 0.9])
+    assert check_columns(df)
 
 
 def test_check_columns_incorrect():
-    df = pandas.DataFrame([0.1, 0.8])
+    df = pd.DataFrame([0.1, 0.8])
     assert check_columns(df) is False
